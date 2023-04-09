9cd8b979a1e0babfb249b7321fd3189e8f2573a8
Fiza Sehar <115126922+fizasehar03@users.noreply.github.com>
2023-04-09 18:20:26 -0400

    Added tests for cities and star network, fixes #9

--- island/src/test/java/IslandTest.java
+++ island/src/test/java/IslandTest.java
@@ -1,24 +1,29 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import ca.mcmaster.cas.se2aa4.a2.io.MeshFactory;
-import ca.mcmaster.cas.se2aa4.a3.island.IslandGenerator;
 import ca.mcmaster.cas.se2aa4.a3.island.elevationprofiles.HillsElevation;
 import ca.mcmaster.cas.se2aa4.a3.island.elevationprofiles.MountainElevation;
 import ca.mcmaster.cas.se2aa4.a3.island.extentionpoints.Aquifers;
+import ca.mcmaster.cas.se2aa4.a3.island.extentionpoints.Cities.Cities;
+import ca.mcmaster.cas.se2aa4.a3.island.extentionpoints.Cities.CityEdge;
+import ca.mcmaster.cas.se2aa4.a3.island.extentionpoints.Cities.Coordinate;
 import ca.mcmaster.cas.se2aa4.a3.island.extentionpoints.Lakes;
 import ca.mcmaster.cas.se2aa4.a3.island.extentionpoints.Rivers;
 import ca.mcmaster.cas.se2aa4.a3.island.shapes.CircleIsland;
-import ca.mcmaster.cas.se2aa4.a3.island.soilabsorption.DrySoil;
+import ca.mcmaster.cas.se2aa4.a4.pathfinder.DijkstraPathFinder;
+import ca.mcmaster.cas.se2aa4.a4.pathfinder.Graph;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ca.mcmaster.cas.se2aa4.a2.io.Structs;
 
 import java.io.IOException;
+import java.sql.Struct;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 
-public class islandTest {
+public class IslandTest {
     private ArrayList<Structs.Polygon> temp;
     private ArrayList<String> type;
     private int numRivers;
@@ -186,5 +191,132 @@ public class islandTest {
 
         assertNotEquals(me1.getElevations(), me3.getElevations());
 
+    }
+
+    @Test
+    public void testExtractPolygonCoordinates() {
+        // Create a sample Mesh object with some vertices and polygons
+        Structs.Mesh.Builder meshBuilder = Structs.Mesh.newBuilder();
+        meshBuilder.addVertices(Structs.Vertex.newBuilder().setX(1).setY(1).build());
+        meshBuilder.addVertices(Structs.Vertex.newBuilder().setX(2).setY(2).build());
+        meshBuilder.addVertices(Structs.Vertex.newBuilder().setX(3).setY(3).build());
+        meshBuilder.addPolygons(Structs.Polygon.newBuilder().setCentroidIdx(0).build());
+        meshBuilder.addPolygons(Structs.Polygon.newBuilder().setCentroidIdx(1).build());
+        Structs.Mesh mesh = meshBuilder.build();
+
+        // Call the extractPolygonCoordinates method
+        HashSet<Coordinate> polygonCoordinates = Cities.extractPolygonCoordinates(mesh);
+
+        // Assert that the returned HashSet contains the expected coordinates
+        assertEquals(2, polygonCoordinates.size());
+        assertTrue(polygonCoordinates.contains(new Coordinate(1.0, 1.0)));
+        assertTrue(polygonCoordinates.contains(new Coordinate(2.0, 2.0)));
+    }
+
+    @Test
+    public void testFindTouchingLandCoordinates() {
+        // Create a sample Mesh object with some vertices, polygons, and segments
+        Structs.Mesh.Builder meshBuilder = Structs.Mesh.newBuilder();
+        meshBuilder.addVertices(Structs.Vertex.newBuilder().setX(1).setY(1).build());
+        meshBuilder.addVertices(Structs.Vertex.newBuilder().setX(2).setY(2).build());
+        meshBuilder.addVertices(Structs.Vertex.newBuilder().setX(3).setY(3).build());
+        meshBuilder.addPolygons(Structs.Polygon.newBuilder().addSegmentIdxs(0).build());
+        meshBuilder.addPolygons(Structs.Polygon.newBuilder().addSegmentIdxs(1).build());
+        meshBuilder.addSegments(Structs.Segment.newBuilder().setV1Idx(0).setV2Idx(1).build());
+        meshBuilder.addSegments(Structs.Segment.newBuilder().setV1Idx(1).setV2Idx(2).build());
+        Structs.Mesh mesh = meshBuilder.build();
+
+        // Create a list of polygon types
+        List<String> types = new ArrayList<>();
+        types.add("land");
+        types.add("water");
+
+        // Call the findTouchingLandCoordinates method
+        HashSet<Coordinate> touchingLand = Cities.findTouchingLandCoordinates(mesh, types);
+
+        // Assert that the returned HashSet contains the expected coordinates
+        assertEquals(2, touchingLand.size());
+        assertTrue(touchingLand.contains(new Coordinate(1.0, 1.0)));
+        assertTrue(touchingLand.contains(new Coordinate(2.0, 2.0)));
+    }
+
+    @Test
+    public void testFilterCities() {
+        // Create a list of vertices
+        List<Structs.Vertex> vertices = new ArrayList<>();
+        vertices.add(Structs.Vertex.newBuilder().setX(1).setY(1).build());
+        vertices.add(Structs.Vertex.newBuilder().setX(2).setY(2).build());
+        vertices.add(Structs.Vertex.newBuilder().setX(3).setY(3).build());
+
+        // Create a HashSet of polygonCoordinates
+        HashSet<Coordinate> polygonCoordinates = new HashSet<>();
+        polygonCoordinates.add(new Coordinate(1.0, 1.0));
+
+        // Create a HashSet of touchingLand
+        HashSet<Coordinate> touchingLand = new HashSet<>();
+        touchingLand.add(new Coordinate(3.0, 3.0));
+
+        // Call the filterCities method
+        HashSet<Coordinate> filteredCities = Cities.filterCities(vertices, polygonCoordinates, touchingLand);
+
+        // Assert that the returned HashSet contains the expected coordinates
+        assertEquals(1, filteredCities.size());
+        assertTrue(filteredCities.contains(new Coordinate(3.0, 3.0)));
+    }
+
+    @Test
+    public void testFindMostCentralCity() {
+        // Create a HashSet of cities
+        HashSet<Coordinate> cities = new HashSet<>();
+        cities.add(new Coordinate(1.0, 1.0));
+        cities.add(new Coordinate(2.0, 2.0));
+        cities.add(new Coordinate(3.0, 3.0));
+
+        // Call the findMostCentralCity method
+        Coordinate mostCentralCity = Cities.findMostCentralCity(cities);
+
+        // Assert that the returned Coordinate is the expected most central city
+        assertEquals(new Coordinate(2.0, 2.0), mostCentralCity);
+    }
+
+    @Test
+    public void testComputeShortestPathsAndConstructCityGraph() {
+        // Create a sample Mesh object with some vertices, polygons, and segments
+        Structs.Mesh.Builder meshBuilder = Structs.Mesh.newBuilder();
+        meshBuilder.addVertices(Structs.Vertex.newBuilder().setX(1).setY(1).build());
+        meshBuilder.addVertices(Structs.Vertex.newBuilder().setX(2).setY(2).build());
+        meshBuilder.addVertices(Structs.Vertex.newBuilder().setX(3).setY(3).build());
+        meshBuilder.addPolygons(Structs.Polygon.newBuilder().addSegmentIdxs(0).build());
+        meshBuilder.addPolygons(Structs.Polygon.newBuilder().addSegmentIdxs(1).build());
+        meshBuilder.addSegments(Structs.Segment.newBuilder().setV1Idx(0).setV2Idx(1).build());
+        meshBuilder.addSegments(Structs.Segment.newBuilder().setV1Idx(1).setV2Idx(2).build());
+        Structs.Mesh mesh = meshBuilder.build();
+
+        // Create a HashSet of allCities
+        HashSet<Coordinate> allCities = new HashSet<>();
+        allCities.add(new Coordinate(1.0, 1.0));
+        allCities.add(new Coordinate(2.0, 2.0));
+        allCities.add(new Coordinate(3.0, 3.0));
+
+        // Call the constructCityGraph method
+        Graph<Coordinate, CityEdge> graph = Cities.constructCityGraph(mesh, allCities, mesh.getPolygonsList(), mesh.getVerticesList());
+
+        // Define the most central city
+        Coordinate mostCentralCity = new Coordinate(2.0, 2.0);
+
+        // Call the computeShortestPaths method
+        DijkstraPathFinder<Coordinate, CityEdge> pathfinder = Cities.computeShortestPaths(graph, mostCentralCity);
+
+        // Assert that the shortest paths are as expected
+        List<Coordinate> pathToCity1 = pathfinder.shortestPath(new Coordinate(1.0, 1.0));
+        List<Coordinate> pathToCity3 = pathfinder.shortestPath(new Coordinate(3.0, 3.0));
+
+        assertEquals(2, pathToCity1.size());
+        assertEquals(mostCentralCity, pathToCity1.get(0));
+        assertEquals(new Coordinate(1.0, 1.0), pathToCity1.get(1));
+
+        assertEquals(2, pathToCity3.size());
+        assertEquals(mostCentralCity, pathToCity3.get(0));
+        assertEquals(new Coordinate(3.0, 3.0), pathToCity3.get(1));
     }
 }
