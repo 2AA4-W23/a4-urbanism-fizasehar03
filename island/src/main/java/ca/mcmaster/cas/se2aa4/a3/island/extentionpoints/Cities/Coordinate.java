e5768e45e2293dc9a388898b3bf4667dd762bec0
Fiza Sehar <115126922+fizasehar03@users.noreply.github.com>
2023-04-10 19:02:16 -0400

    Revert "Added tests for cities and star network, fixes #9"
    This reverts commit 8c518cb9d6edc047816c647c64bacbc0997ec615.

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/extentionpoints/Cities/Coordinate.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/extentionpoints/Cities/Coordinate.java
@@ -5,55 +5,55 @@ import ca.mcmaster.cas.se2aa4.a2.io.Structs;
 import java.util.Objects;
 
 public class Coordinate {
-    Double x;
-    Double y;
+        Double x;
+        Double y;
 
-    public Coordinate(Structs.Vertex v) {
-        this.x = v.getX();
-        this.y = v.getY();
-    }
-
-    public Coordinate(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public double getDistance(Coordinate other) {
-        double pointDifference = Math.sqrt((Math.pow(this.getX()-other.getX(),2)+Math.pow(this.getY()-other.getY(),2)));
-        return pointDifference;
-    }
-
-    public double getX() {
-        return x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public Structs.Vertex makeVertex() {
-        return Structs.Vertex.newBuilder().setX(x).setY(y).build();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
+        Coordinate(Structs.Vertex v) {
+            this.x = v.getX();
+            this.y = v.getY();
         }
-        if (obj == null || getClass() != obj.getClass()) {
-            return false;
+
+        Coordinate(double x, double y) {
+            this.x = x;
+            this.y = y;
         }
-        Coordinate that = (Coordinate) obj;
-        return Double.compare(that.x, x) == 0 && Double.compare(that.y, y) == 0;
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
-    }
+        public double getDistance(Coordinate other) {
+            double pointDifference = Math.sqrt((Math.pow(this.getX()-other.getX(),2)+Math.pow(this.getY()-other.getY(),2)));
+            return pointDifference;
+        }
 
-    @Override
-    public String toString() {
+        public double getX() {
+            return x;
+        }
+
+        public double getY() {
+            return y;
+        }
+
+        public Structs.Vertex makeVertex() {
+            return Structs.Vertex.newBuilder().setX(x).setY(y).build();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null || getClass() != obj.getClass()) {
+                return false;
+            }
+            Coordinate that = (Coordinate) obj;
+            return Double.compare(that.x, x) == 0 && Double.compare(that.y, y) == 0;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+
+        @Override
+        public String toString() {
             return this.x.toString() + "," + this.y.toString();
         }
 }
