ffed6406be44db7524440b4662f4abe6172eb0bc
Fiza Sehar <115126922+fizasehar03@users.noreply.github.com>
2023-04-10 19:02:43 -0400

    Revert "Improved code readability"
    This reverts commit 86e96169a684dc76f2aaddd91fd17a6ae5c88d0f.

--- island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/extentionpoints/Cities/CitySize.java
+++ island/src/main/java/ca/mcmaster/cas/se2aa4/a3/island/extentionpoints/Cities/CitySize.java
@@ -3,25 +3,25 @@ package ca.mcmaster.cas.se2aa4.a3.island.extentionpoints.Cities;
 import java.util.Random;
 
 
-    public enum CitySize {
-        TOWN(1),
-        SMALL(2),
-        MEDIUM(3),
-        LARGE(4),
-        METROPOLIS(5);
+public enum CitySize {
+    TOWN(1),
+    SMALL(2),
+    MEDIUM(3),
+    LARGE(4),
+    METROPOLIS(5);
 
-        private final int index;
+    private final int index;
 
-        private CitySize(int index) {
-            this.index = index;
-        }
-
-        public int getIndex() {
-            return index;
-        }
-
-        public static CitySize getRandomCitySize(Random random) {
-            return values()[random.nextInt(values().length)];
-        }
+    private CitySize(int index) {
+        this.index = index;
     }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public static CitySize getRandomCitySize(Random random) {
+        return values()[random.nextInt(values().length)];
+    }
+}
 
